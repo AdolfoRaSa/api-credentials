f463787d548cec56d5143c86e239262edea85748
Adolfo <adramirezsa@isseg.gob.mx>
2024-01-16 11:02:43 -0600

    Few changes

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,4 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 var _app = _interopRequireDefault(require("./app"));
 _app["default"].listen(_app["default"].get("port"));
-console.log("Listening on port ", _app["default"].get("port"));
+console.log("Listening on port: ", _app["default"].get("port"));
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import app from "./app";
 
 app.listen(app.get("port"));
 
-console.log("Listening on port ", app.get("port"));
+console.log("Listening on port: ", app.get("port"));
